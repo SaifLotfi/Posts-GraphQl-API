f5c8bdf22a17e6e840d2b6f851a29af2c9074d44
Saif <swaifweaa@gmail.com>
2023-08-06 15:03:39 +0300

    storing Posts in Database

--- controller/feed.js
+++ controller/feed.js
@@ -1,21 +1,44 @@
 const express = require('express');
-
+const Post = require('../models/post');
+const {validationResult} = require('express-validator')
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        title: 'First Post',
-        content: 'This is my First Post!',
+        posts: [
+            {
+                _id: '1',
+                title: 'First Post',
+                content: 'This is the first post!',
+                imageUrl: 'images/duck.jpg',
+                creator: {
+                    name: 'Saif',
+                },
+                createdAt: new Date(),
+            },
+        ],
     });
 };
 
 exports.postPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            message:errors.array()[0].msg,
+            errors:errors.array()
+        })
+    }
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'The Post Has Been Created Successfully!',
-        post: {
-            id:Date.now().toString(),
-            title: title,
-            content: content,
-        },
+    const post = new Post({
+        title:title,
+        content:content,
+        imageUrl:'images/monkey',
+        creator:{name:'Saif'}
     });
+    post.save().then(post=>{
+        res.status(201).json({
+        message: 'The Post Has Been Created Successfully!',
+        post: post
+    });
+    }).catch(err=>console.log(err));
+    
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,20 @@
+const { body } = require('express-validator');
+
 const feedControllers = require('../controller/feed');
 
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/posts',feedControllers.getPosts);
+router.get('/posts', feedControllers.getPosts);
 
-router.post('/post',feedControllers.postPost);
+router.post(
+    '/post',
+    [
+        body('title', 'Invalid Title!').trim().isLength({ min: 5 }),
+        body('content', 'Invalid Content!').trim().isLength({ min: 5 }),
+    ],
+    feedControllers.postPost
+);
 
 module.exports = router;
-
-
